e571fcd59d0d2b2cb140dcaa3d94a9876ca593a4
AndreLucasv10 <andrelucasv10@gmail.com>
2023-08-11 13:20:00 -0300

    Estudo basico sobre JavaScript

---
+++ JavaScript basico/script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector("nav");
+const produto = document.querySelector(".produto a");
+
+console.log(nav);
+console.log(produto);
+
+console.log(produto.href);
+
+nav.style.backgroundColor = "black";
+nav.style.padding = "1rem";
+nav.classList.add("ativo");
+nav.classList.add("ativo");
